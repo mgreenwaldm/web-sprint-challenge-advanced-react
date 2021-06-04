ce5aa898f97b3f885b3b120e6987b36e1e28768e
Michael Greenwald <mgreenwaldm@icloud.com>
2021-06-04 12:56:23 -0500

    plantlist done

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -8,44 +8,44 @@ export default class PlantList extends Component {
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state ={[]};
+    this.state = {plants: []};
   }
 
-  /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
+  /**  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION **/
   componentDidMount() {
-    axios.get('http://localhost:3333/plants')
-      .then(res => {
-        this.setState({})
-      });
+    axios.get('http://localhost:3333/plants%27)
+        .then(res => {
+          this.setState({plants: res.data})
+        });
   }
 
   render() {
     return (
-      <main className="plant-list">
-        {this.state?.plants?.map((plant) => (
-          <div className="plant-card" key={plant.id} data-testid="plant-card">
-            <img className="plant-image" src={plant.img} alt={plant.name} />
-            <div className="plant-details">
-              <h2 className="plant-name">{plant.name}</h2>
-              <p className="plant-scientific-name">{plant.scientificName}</p>
-              <p>{plant.description}</p>
-              <div className="plant-bottom-row">
-                <p>${plant.price}</p>
-                <p>☀️ {plant.light}</p>
-                <p>💦 {plant.watering}x/month</p>
+        <main className="plant-list">
+          {this.state?.plants?.map((plant) => (
+              <div className="plant-card" key={plant.id} data-testid="plant-card">
+                <img className="plant-image" src={plant.img} alt={plant.name} />
+                <div className="plant-details">
+                  <h2 className="plant-name">{plant.name}</h2>
+                  <p className="plant-scientific-name">{plant.scientificName}</p>
+                  <p>{plant.description}</p>
+                  <div className="plant-bottom-row">
+                    <p>${plant.price}</p>
+                    <p> {plant.light}</p>
+                    <p> {plant.watering}x/month</p>
+                  </div>
+                  <button
+                      className="plant-button"
+                      onClick={() => this.props.addToCart(plant)}
+                  >
+                    Add to cart
+                  </button>
+                </div>
               </div>
-              <button
-                className="plant-button"
-                onClick={() => this.props.addToCart(plant)}
-              >
-                Add to cart
-              </button>
-            </div>
-          </div>
-        ))}
-      </main>
+          ))}
+        </main>
     );
   }
 }
